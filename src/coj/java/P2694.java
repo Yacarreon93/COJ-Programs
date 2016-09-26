fd7d34596b41664c10840101f1b8fa4d06ea7b04
Yasser Carreón <yacasia@gmail.com>
2016-09-26 11:48:30 -0500

    Success 2694 More Difficult to Solve
    Try 1 - 988906 Accepted
    
    http://coj.uci.cu/24h/problem.xhtml?pid=2694
    http://coj.uci.cu/24h/submission.xhtml?id=988906

---
+++ src/coj/java/P2694.java
@@ -0,0 +1,14 @@
+package coj.java;
+
+/*
+2694 - More Difficult to Solve
+Description: http://coj.uci.cu/24h/problem.xhtml?pid=2694
+*/
+
+public class P2694 {
+        
+    public static void main(String[] args) {
+        System.out.println("This is my first solution in programming competitions");
+    }
+    
+}
