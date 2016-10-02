d247022a48e3616a891b154fda72a61091c442ea
Yasser Carreón <yacasia@gmail.com>
2016-10-02 16:29:03 -0500

    Failed 1808 Hamming Distance
    Try 1 - 993759 Wrong Answer
    
    http://coj.uci.cu/24h/problem.xhtml?pid=1808
    http://coj.uci.cu/24h/submission.xhtml?id=993759

---
+++ src/coj/java/P1808.java
@@ -0,0 +1,40 @@
+package coj.java;
+
+/*
+1808 - Hamming Distance
+Description: http://coj.uci.cu/24h/problem.xhtml?pid=1808
+*/
+
+import java.util.Scanner;
+
+public class P1808 {
+    
+    private static Scanner sc = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        
+        String s1 = sc.next();
+        
+        while(!s1.equals("X")) {
+            
+            short distance = 0;
+            
+            String s2 = sc.next();
+            
+            for (short i = 0; i < s2.length(); i++) {
+                
+                if(s2.charAt(i) != s1.charAt(i)) {
+                    distance++;
+                }
+                
+            }
+            
+            System.out.println("Hamming distance is " + distance);
+            
+            s1 = sc.next();
+            
+        }
+        
+    }
+    
+}
