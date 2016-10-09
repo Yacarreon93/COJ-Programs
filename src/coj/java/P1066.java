6097f6e45af579d3248f0cfd1c6407ffbf30e549
Yasser Carreón <yacasia@gmail.com>
2016-10-09 11:03:45 -0500

    Success 1066 Regular Polygons
    Try 1 - 999416 Accepted
    
    http://coj.uci.cu/24h/problem.xhtml?pid=1066
    http://coj.uci.cu/24h/submission.xhtml?id=999416

---
+++ src/coj/java/P1066.java
@@ -0,0 +1,34 @@
+package coj.java;
+
+/*
+1066 - Regular Polygons
+Description: http://coj.uci.cu/24h/problem.xhtml?pid=1066
+*/
+
+import java.util.Scanner;
+
+public class P1066 {    
+     
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        final double PI = 3.141592653589793;
+        
+        int testCases = sc.nextInt();
+        
+        for (int i = 0; i < testCases; i++) {
+            
+            double R = sc.nextDouble();
+            byte N = sc.nextByte();
+            
+            double inscribed_perimeter = 2 * N * R * Math.sin(PI / N);
+            double circumscribed_perimeter = 2 * N * R * Math.tan(PI / N);           
+            
+            System.out.format("%.4f %.4f\n", inscribed_perimeter, circumscribed_perimeter);
+            
+        }
+        
+    }
+    
+}
